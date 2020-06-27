8d3ca54fe0bca64960ab05c888beb01552a3db08
Travis Bumgarner <travis.bumgarner@gmail.com>
2020-06-27 17:03:43 -0400

    Show and tell and office hours output to terminal, Added timing for advertisement

--- src/commands.ts
+++ src/commands.ts
@@ -13,11 +13,11 @@ type Message = {
     userId: string
 }
 
-const showAndTellMessages: Message[] = [] // Replace with GCloud
-const officeHoursMessages: Message[] = [] // Replace with GCloud
+const handleMessage = (message: Message) => {
+    console.log('\x1b[30m\x1b[43m%s\x1b[0m', JSON.stringify(message))
+}
 
 const hello = (client: Client, { userState, channel }: CommandArguments) => {
-    console.log(userState)
     const response = `Hello to you too, ${userState.username} we're hot reloading now. It's the future!`
     client.say(channel, response)
 }
@@ -25,7 +25,7 @@ const hello = (client: Client, { userState, channel }: CommandArguments) => {
 const showAndTell = (client: Client, { userState, channel, message }: CommandArguments) => {
     let response: string
     if (message && userState["user-id"] && userState.username) {
-        showAndTellMessages.push({ message, userId: userState["user-id"], username: userState.username })
+        handleMessage({ message, userId: userState["user-id"], username: userState.username })
         response = `Thank you ${userState["display-name"]}! Your show and tell idea has been received.`
     } else {
         response = 'Sorry, there was an issue with your request. Please suply a message along with your command.'
@@ -36,7 +36,7 @@ const showAndTell = (client: Client, { userState, channel, message }: CommandArg
 const officeHours = (client: Client, { userState, channel, message }: CommandArguments) => {
     let response: string
     if (message && userState["user-id"] && userState.username) {
-        officeHoursMessages.push({ message, userId: userState["user-id"], username: userState.username })
+        handleMessage({ message, userId: userState["user-id"], username: userState.username })
         response = `Thank you ${userState["display-name"]}! Your office hours request has been received.`
     } else {
         response = 'Sorry, there was an issue with your request. Please suply a message along with your command.'
--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ const onMessageHandler = (channel: string, userState: ChatUserstate, rawInput: s
     if (command === null || command in VALID_COMMANDS) {
         return null
     }
-    
+
     const commandArguments = { userState, command, message, channel }
     switch (command as ValidCommands) {
         case '!hello':
@@ -33,8 +33,24 @@ const onMessageHandler = (channel: string, userState: ChatUserstate, rawInput: s
     }
 }
 
+const advertise = () => {
+    const now = new Date()
+    if (now.getMinutes() === 30) {
+        client.say(
+            config.channels[0],
+            "Need help on a project? Type \"!officehours your message\" to get help live!"
+        )
+    } else if (now.getMinutes() === 0) {
+        client.say(
+            config.channels[0],
+            "Have a project to share? Type \"!showandtell your project\" to share it live!"
+        )
+    }
+}
+
 const onConnectedHandler = (address: string, port: number) => {
     console.log(`* Connected to ${address}: ${port} `);
+    setInterval(advertise, 1000 * 60)
 }
 
 client.on('message', onMessageHandler);
